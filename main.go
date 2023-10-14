3a12fb6c3f497da11b115b8c92407abed5d704e0
Matheus Cruz <matheuscruz.dev@gmail.com>
2023-10-14 19:52:26 -0300

    refactor: refactor code

--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"io"
-	"log"
 	"net/http"
 	"os"
 	"strings"
@@ -18,33 +16,22 @@ func main() {
 	targetUrl := os.Getenv("TARGET_URL")
 	description := os.Getenv("DESCRIPTION")
 
-	entries, err := os.ReadDir("/etc/gh-checkmoon")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, e := range entries {
-		fmt.Println(e.Name())
-	}
-
 	token, err := os.ReadFile("/etc/gh-checkmoon/token")
 
 	if err != nil {
 		panic(any(err))
 	}
 
-	log.Println(string(token))
-
 	requestBody := []byte(fmt.Sprintf(`{
 		"state": "%s",
 		"target_url": "%s",
 		"description": "%s",
-		"context": "continuous-integration/platformoon"
+		"context": "build"
 	}`, state, targetUrl, description))
 
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(requestBody))
 	if err != nil {
-		panic(fmt.Errorf("error while creating request"))
+		panic(any(err))
 	}
 
 	req.Header.Set("Accept", "application/vnd.github+json")
@@ -58,7 +45,4 @@ func main() {
 		panic(any(err))
 	}
 	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-
-	log.Println(string(body))
 }
